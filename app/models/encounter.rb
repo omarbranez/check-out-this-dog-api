4f2a825145bdc75c4d33651a8268dbc806dd82bb
Omar <omar.branez@gmail.com>
2021-10-18 16:26:43 -0400

    add photos from activestorage to serializer

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -2,4 +2,8 @@ class Encounter < ApplicationRecord
   belongs_to :user
   belongs_to :dog
   has_one_attached :photo
+
+  def photo_url
+    Rails.application.routes.url_helpers.url_for(photo) if photo.attached?
+  end
 end
