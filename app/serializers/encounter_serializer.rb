52fff50042de2b0fe611006fb948ad66f87e272c
Omar <omar.branez@gmail.com>
2021-10-29 16:26:27 -0400

    add date to report serializer

--- app/serializers/encounter_serializer.rb
+++ app/serializers/encounter_serializer.rb
@@ -2,7 +2,7 @@ class EncounterSerializer < ActiveModel::Serializer
 include Rails.application.routes.url_helpers
   # include JSONAPI::Serializer
   # set_key_transform :camel_lower
-  attributes :id, :username, :breed, :name, :color, :age, :features, :demeanor, :lat, :lng, :user_id, :dog_id, :photo
+  attributes :id, :username, :breed, :name, :color, :age, :features, :demeanor, :lat, :lng, :user_id, :dog_id, :photo, :created, :updated_at
   
   def photo
     if object.photo.attached?
@@ -16,6 +16,10 @@ include Rails.application.routes.url_helpers
   
   def breed
     Dog.find(object.dog_id).breed
+  end
+
+  def created
+    object.created_at.strftime("%I:%M %p, %B %d, %Y")
   end
   # attribute :photo do |encounter|
   #   encounter.photo_url
